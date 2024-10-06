aa652897b86a61b31cfadab323192c600746c056
Hector Carmona <orcabe86@gmail.com>
2024-10-06 10:00:50 -0600

    Initial Commit

---
+++ MusicHub/MusicHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicHubApp.swift
+//  MusicHub
+//
+//  Created by Hector Carmona on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
